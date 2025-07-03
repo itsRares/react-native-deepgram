48f8850a533ebb6884e843ab242cbc3aebe5997e
Rares <rarpopa@hotmail.com>
2025-07-03 07:58:29 -0400

    feat(example): switch to Expo

--- example/app.config.js
+++ example/app.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  name: 'DeepgramExample',
-  android: {
-    package: 'com.itsrares.deepgramexample',
-  },
-  ios: {
-    bundleIdentifier: 'com.itsrares.deepgramexample',
-  },
-  plugins: [
-    [
-      'react-native-deepgram',
-      {
-        microphonePermission:
-          'This app needs microphone access for speech recognition',
-      },
+  expo: {
+    name: 'DeepgramExample',
+    android: {
+      package: 'com.itsrares.deepgramexample',
+    },
+    ios: {
+      bundleIdentifier: 'com.itsrares.deepgramexample',
+    },
+    plugins: [
+      [
+        'react-native-deepgram',
+        {
+          microphonePermission:
+            'This app needs microphone access for speech recognition',
+        },
+      ],
     ],
-  ],
+  },
 };
